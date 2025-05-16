d9faa997310bb2a2f48c026164489b287e9ea25a
marindave <marindave856@gmail.com>
2025-05-16 09:07:54 +0530

    Time: 5 ms (38.91%), Space: 45.5 MB (66.6%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int[] ans=new int[32];
+        int temp=0;
+        for(int i=31;i>=0;i--){
+            int count=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[j]%2!=0) ans[i]=++count;
+                nums[j]=nums[j]>>1;
+            }
+            
+        }
+      // now this part is important because wer are reconstruting our unique number;
+    int result = 0;
+    for (int i = 0; i < 32; i++) {
+        result = (result << 1) | (ans[i] % 3);
+    }
+
+    return result;
+    }
+}
