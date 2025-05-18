ef2519bbc271849b7224c24cda90e22b428fee86
marindave <marindave856@gmail.com>
2025-05-18 11:56:56 +0530

    Time: 2 ms (95.72%), Space: 46.1 MB (21.96%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,24 +1,31 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        Arrays.sort(nums);
-        int[] ans=new int[2];
-        int n=nums.length;
-        int expectedSum=(int) n*(n+1)/2;
-        int s=0;
-        int e=1;
-        while(e<n){
-            if((nums[s]^nums[e])==0){
-                ans[0]=nums[s];
-                break;
-            }
-            s++;
-            e++;
-        }
-        int actualSum=0;
-        for(int i=0;i<nums.length;i++){
-        actualSum=actualSum+nums[i];
+       //let us just do this question only by bit manipulation
+       int xor1=0;
+       int xor2=0;
+       for(int i=0;i<nums.length;i++){
+        xor2=xor2^nums[i];
        }
-       ans[1]=expectedSum-(actualSum-ans[0]);
-       return ans;
+       for(int i=1;i<=nums.length;i++){
+        xor1=xor1^i;
+       }
+       int xor=xor1^xor2;
+       //till this you need to understand that the xor is containing the xor of repeating and duplicate number
+       // so our next aim is to just partition this xor into respective repeating and duplicate number
+    int temp=((xor)&(-xor));//temp is having the rightmostbit
+    int x=0;
+    int y=0;
+    for(int i=0;i<nums.length;i++){
+        if((nums[i]&temp)==0) x=x^nums[i];
+        else y=y^nums[i];
+    }
+    for(int i=1;i<=nums.length;i++){
+        if((i&temp)==0) x=x^i;
+        else y=y^i;
+    }
+     for(int i=0;i<nums.length;i++){
+        if(nums[i]==x) return new int[]{x,y};
+     }
+     return new int[]{y,x};
     }
 }
