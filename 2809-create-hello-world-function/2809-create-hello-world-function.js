d4ec4d9dff2e9169b485665008a7b351475fffa6
marindave <marindave856@gmail.com>
2025-05-24 12:06:17 +0530

    Time: 30 ms (96.47%), Space: 53.4 MB (43.78%) - LeetHub

--- 2809-create-hello-world-function/2809-create-hello-world-function.js
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -1,6 +1,7 @@
 /**
  * @return {Function}
  */
+ /** 
     function createHelloWorld(){
     return function(){
         return "Hello World";
@@ -8,6 +9,13 @@
 }
 const f=createHelloWorld();
 f();
+*/
+var createHelloWorld=function(){
+    return function(){
+        return "Hello World";
+    }
+}
+ //var createHelloWorld=()=>()=>"Hello Wolrd";
 
 /**
  * const f = createHelloWorld();
